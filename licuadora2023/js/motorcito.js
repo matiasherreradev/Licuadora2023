e0a7e01c2e2268af4d2fdf361a9dc292c5e7c572
senderos3d <senderos3d@gmail.com>
2023-05-31 01:09:02 -0300

    repo

---
+++ licuadora2023/js/motorcito.js
@@ -0,0 +1,29 @@
+var estadoLicuadora = "apagada";
+var  sonidoLicuadora = document.getElementById("blender-sound")
+var botonLicuadora = document.getElementById("blender-button-sound")
+var licuadora = document.getElementById("blender");
+
+function controlarLicuadora() {
+    if (estadoLicuadora =="apagada") {
+        estadoLicuadora = "encendido";
+        hacerBrrBrr();
+        licuadora.classList.add("active");
+        // console.log("me prendiste");
+    } else {
+        estadoLicuadora = "apagada";
+        licuadora.classList.remove("active");
+        // console.log("me apagaste");
+        
+    }
+}
+
+function hacerBrrBrr() {
+    if (sonidoLicuadora.pause) {
+        botonLicuadora.play();
+        sonidoLicuadora.play();
+    } else {
+        botonLicuadora.play();
+        sonidoLicuadora.pause();
+        sonidoLicuadora.currentTime= 0;       
+    }
+}
